b59e83645d2d9d00d2c1c36993dbf59c357afd15
oriapp <plbwymw@gmail.com>
2023-10-02 00:16:54 +0300

    rewriting the JS, readable, and fixed some issues meanwhile.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
-function setDisplayNone(...elements) {
+/**
+ * 
+ * @param {*} style 
+ * @param  {...any} elements 
+ */
+function setStyleDisplay(style, ...elements) {
         elements.forEach(element => {
-                element.style.display = "none";
+                element.style.display = style;
         });
 }
 
 var arr = [];
 for (var i = 1; i <= 5; i++) arr[i] = 'q' + i;
-for (var i = 1; i <= 5; i++) setDisplayNone(document.getElementById(arr[i]));
+for (var i = 1; i <= 5; i++) setStyleDisplay("none", document.getElementById(arr[i]));
 var res = document.getElementById("res");
 var introduction = document.getElementById("introduction");
 var showproblems = document.getElementById('showproblems');
 var befres = document.getElementById('befres');
 var countdown = document.getElementById('countdown');
 
-setDisplayNone(res, introduction, showproblems, befres, countdown);
+setStyleDisplay("none", res, introduction, showproblems, befres, countdown);
 
 
 var attempt = [0, 0, 0, 0, 0];
@@ -21,88 +26,83 @@ var attemp = [0, 0, 0, 0, 0];
 var score = [0, 0, 0, 0, 0];
 var scoreAc = [0, 0, 0, 0, 0];
 
-
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ */
 function addScore(a, b) {
         score[a] = b;
         attemp[a] = 1;
 }
 
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ */
 function showPrev(a, b) {
-        setDisplayNone(document.getElementById(a));
-        document.getElementById(b).style.display = "block";
+        setStyleDisplay("none", document.getElementById(a));
+        setStyleDisplay("block", document.getElementById(b));
 }
+
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ */
 function showQue(a, b) {
-        if (b == 'introduction') setDisplayNone(document.getElementById("callByName"));
-        else document.getElementById("callByName").style.display = "";
-
-        if (a == "q1") {
-                if (attemp[0] == 1) {
-                        document.getElementById(a + '1').style.border = "2px solid #0000FF";
-                        attempt[0] = 1;
-                        scoreAc[0] = score[0];
-                }
+        const callByNameElement = document.getElementById("callByName");
+      
+        if (b === 'introduction') {
+          setStyleDisplay("none", callByNameElement);
+        } else {
+          setStyleDisplay("", callByNameElement);
         }
-
-        if (a == "q2") {
-                if (attemp[1] == 1) {
-                        document.getElementById(a + '1').style.border = "2px solid #0000FF";
-                        attempt[1] = 1;
-                        scoreAc[1] = score[1];
-                }
+      
+        const questionIndexes = ["q1", "q2", "q3", "q4", "q5"];
+        const currentIndex = questionIndexes.indexOf(a);
+      
+        if (currentIndex !== -1 && attemp[currentIndex] === 1) {
+          document.getElementById(a + '1').style.border = "2px solid #0000FF";
+          attempt[currentIndex] = 1;
+          scoreAc[currentIndex] = score[currentIndex];
         }
-
-        if (a == "q3") {
-                if (attemp[2] == 1) {
-                        document.getElementById(a + '1').style.border = "2px solid #0000FF";
-                        attempt[2] = 1;
-                        scoreAc[2] = score[2];
-                }
+      
+        if (b === "q1") {
+          document.getElementById("body").style.width = "75%";
+          setStyleDisplay("block", document.getElementById('showproblems'));
         }
-
-        if (a == "q4") {
-                if (attemp[3] == 1) {
-                        document.getElementById(a + '1').style.border = "2px solid #0000FF";
-                        attempt[3] = 1;
-                        scoreAc[3] = score[3];
-                }
+      
+        if (a === 'befres' && b === 'res') {
+          if (document.getElementById('check').checked) {
+            setStyleDisplay("none", document.getElementById(a));
+            setStyleDisplay("block", document.getElementById(b));
+            time = Infinity;
+            setStyleDisplay("none", countdown);
+          } else {
+            alert('Kindly check the checkbox before clicking the submit button');
+          }
+        } else {
+          setStyleDisplay("none", document.getElementById(a));
+          setStyleDisplay("block", document.getElementById(b));
         }
-
-        if (a == "q5") {
-                if (attemp[4] == 1) {
-                        document.getElementById(a + '1').style.border = "2px solid #0000FF";
-                        attempt[4] = 1;
-                        scoreAc[4] = score[4];
-                }
-        }
-
-        if (b == "q1") {
-                document.getElementById("body").style.width = "75%";
-                document.getElementById('showproblems').style.display = "block";
-        }
-        
-        if (a == 'befres' && b == 'res') {
-                if (document.getElementById('check').checked) {
-                        setDisplayNone(document.getElementById(a));
-                        document.getElementById(b).style.display = "block";
-                        time = Infinity;
-                        setDisplayNone(countdown);
-                }
-                else {
-                        alert('Kindly check the checkbox before clicking the submit button');
-                }
-        }
-        else {
-                setDisplayNone(document.getElementById(a));
-                document.getElementById(b).style.display = "block";
-        }
-}
+      }
+      
 
 var res = 0;
 var attempted = 0;
 
+
 function reload() {
         window.location.reload();
 }
+
+/**
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ */
 function checkinitial(a, b) {
         if (document.getElementById("username").value == "")
                 alert("Please enter your name");
@@ -115,19 +115,25 @@ function checkinitial(a, b) {
                 showQue(a, b);
         }
 }
+
 function showorHide() {
         var introname = document.getElementById("callByName");
         if (document.getElementById("username").value == "")
-                introname.style.dispaly = "none";
+                setStyleDisplay("none", introname);
         else {
-                introname.style; length.display = "block";
+                setStyleDisplay("block", introname)
         }
 }
+
+/**
+ * 
+ * @param {*} a 
+ */
 function showques(a) {
         for (var i = 1; i <= 5; i++)
-                document.getElementById(arr[i]).style.display = "none";
-        document.getElementById(a).style.display = "block";
-        document.getElementById("befres").style.display = "none";
+                setStyleDisplay("none", document.getElementById(arr[i]));
+        setStyleDisplay("block", document.getElementById(a));
+        setStyleDisplay("none", document.getElementById("befres"));
 }
 
 
@@ -136,9 +142,11 @@ let time = startingMinutes * 60;
 
 function startTimer() {
         setInterval(updateCountdown, 1000);
-        document.getElementById('countdown').style.display = "block";
+        setStyleDisplay("block", document.getElementById('countdown'));
 }
+
 var min, sec;
+
 function updateCountdown() {
         const minutes = Math.floor(time / 60);
         let seconds = time % 60;
@@ -146,35 +154,57 @@ function updateCountdown() {
 
         countdown.innerHTML = `${minutes}:${seconds}`;
         time--;
-        if (minutes == 0 && seconds == 0) {
-                alert("Time Up");
-                for (var i = 1; i <= 5; i++) document.getElementById(arr[i]).style.display = "none";
-                document.getElementById("res").style.display = "block";
-                setDisplayNone(introduction, showproblems, befres, countdown);
 
-                min = minutes; sec = seconds;
+        if (minutes === 0 && seconds === 0) {
+                alert("Time Up");
+
+                for (let i = 1; i <= 5; i++) {
+                        document.getElementById(arr[i]).style.display = "none";
+                }
+
+                setStyleDisplay("block", document.getElementById("res"));
+                setStyleDisplay("none", introduction, showproblems, befres, countdown);
+
+                min = minutes;
+                sec = seconds;
                 showScore();
         }
 }
 
 
-function showScore(){
-        if(document.getElementById('check').checked || (min<=0 && sec<= 0)){
-                const container = document.getElementById("board");
+
+function showScore() {
+        const checkElement = document.getElementById('check');
+        const container = document.getElementById("board");
+
+        if (checkElement.checked || (min <= 0 && sec <= 0)) {
                 const fireworks = new Fireworks.default(container);
-                
-                for(var i=0; i<5; i++) res+=scoreAc[i];
-                for(var i=0; i<5; i++) attempted+=attempt[i];
-                document.getElementById("totalQ").innerHTML = 5;
-                document.getElementById("totalS").innerHTML = 5 * res - attempted;
+
+                let res = 0;
+                let attempted = 0;
+                const numQuestions = 5;
+
+                for (let i = 0; i < numQuestions; i++) {
+                        res += scoreAc[i];
+                }
+
+                for (let i = 0; i < numQuestions; i++) {
+                        attempted += attempt[i];
+                }
+
+                document.getElementById("totalQ").innerHTML = numQuestions;
+                document.getElementById("totalS").innerHTML = numQuestions * res - attempted;
                 document.getElementById("attemtQ").innerHTML = attempted;
                 document.getElementById("corrQ").innerHTML = res;
                 document.getElementById("wrongQ").innerHTML = attempted - res;
                 document.getElementById("body").style.width = "100%";
-                if(res >= 3) {
+
+                if (res >= 3) {
                         fireworks.start();
                 }
-                showproblems.style.display = "none";
+
+                setStyleDisplay("none", showproblems);
+
                 console.log(min);
                 console.log(sec);
         }
